49f9d3215e2d225bd39ef4aae443674eaf65f4c9
Shuang Li <s881672000@gmail.com>
2025-04-24 17:24:40 +0800

    feat : router test

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import Home from '../components/Home.vue';
 import Projects from '../components/Projects.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/projects', component: Projects },
+  //{ path: '/', component: Home },
+  { path: '/', component: Projects },
 ];
 
 const router = createRouter({
